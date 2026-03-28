1dbce850ca2b29b5ebfb7593a2d58bdd59099a03
Marioexplo <marioexplo@protonmail.com>
2026-03-28 16:48:03 +0100

    After test
    backup.py completed

--- backup.py
+++ backup.py
@@ -1,12 +1,13 @@
-import importlib.util as py_read
+import json
 from os import path
 import sys
 import typing
 import pyudev
+import os
 import subprocess
 from datetime import datetime
 import locale
-from glob import glob
+from pathspec import PathSpec
 
 def error(message: str)->typing.Never:
     print(message)
@@ -14,74 +15,80 @@ def error(message: str)->typing.Never:
 
 # get config_path
 HOME = path.expanduser("~")
-config_path = HOME + ".config/borXplo.config"
 if "--config" in sys.argv:
     index = sys.argv.index("--config")
     if len(sys.argv) < index:
         error("No config file path was given after '--config'!")
     config_path = sys.argv[index - 1]
+else:
+    config_path = path.join(HOME, ".config/borXplo.json")
 
 # get config
-spec = py_read.spec_from_file_location("config", config_path)
-if not spec:
-    error("No config file was found.\nUse \"borxplo guide\" to get help creating a config file")
-config = py_read.module_from_spec(spec)
-spec.loader.exec_module(config) # pyright: ignore
+seek_help = "\nUse \"borxplo guide\" to get help creating a config file"
+try:
+    with open(config_path) as f:
+        config = json.load(f)
+except OSError as e:
+    error(f"While trying to read {config_path}, this error was raised: {e}{seek_help}")
+except json.JSONDecodeError as e:
+    error("JSON decoder exited with error: " + e.msg + seek_help)
+if type(config) is not dict:
+    error("borXplo.json was not a json object!" + seek_help)
 
-MISTYPE = object()
-def check_option(name: str, typ: type)->bool|typing.Never:
-    """Checks that an option of name 'name' and type 'typ' exists in config."""
-    value = getattr(config, name, MISTYPE)
-    if value is MISTYPE:
-        return False
-    if isinstance(value, typ):
-        return True
-    error(f"{name} must be of  type '{type}'")
+T = typing.TypeVar("T")
+def get_key(key: str, typ: typing.Type[T], d: dict = config) -> T | None | typing.Never:
+    if key not in d:
+        return None
+    if type(d[key]) is typ:
+        return d[key]
+    error(f"{key} must be of  type '{typ}'")
 
 # get storage device
 if "--path" in sys.argv:
     index = sys.argv.index("--path")
     if len(sys.argv) < index:
         error("No path was given after '--path'!")
-    target_path = sys.argv[index - 1]
+    target_path = sys.argv[index + 1]
 else:
     device_database = pyudev.Context()
-    if check_option("target_path", str):
-        if path.exists(config.target_path):
+    target_dir = get_key("target_path", str)
+    if target_dir is not None:
+        if path.exists(target_dir):
             error("target_path doesn't seem to be a real path")
         try:
-            device = pyudev.Devices.from_device_file(device_database, config.target_path)
+            device = pyudev.Devices.from_device_file(device_database, target_dir)
         except pyudev.DeviceNotFoundError:
-            error("No device was found at " + config.target_path)
-    elif check_option("target_label", str):
+            error("No device was found at " + target_dir)
+    else:
+        target_label = get_key("target_label", str)
+        if target_label is None:
+            error("target_label or target_path must be given to find your device")
         subsystem = "block" if getattr(config, "only_usb", True) else "usb"
         devices: list[pyudev.Device] = list()
         key = "ID_FS_LABEL"
         for device in device_database.list_devices(subsystem=subsystem):
-            if key in device.properties and device.properties[key] == config.target_label:
+            if key in device.properties and device.properties[key] == target_dir:
                 devices.append(device)
         match len(devices):
             case 0:
-                error(f"No device starting with '{config.target_label}' was found")
+                error(f"No device starting with '{target_dir}' was found")
             case 1:
                 device = devices[0]
             case _:
-                print(f"More than one device labelled '{config.target_label}' was found.\nChoose one:")
+                print(f"More than one device labelled '{target_dir}' was found.\nChoose one:")
                 while True:
                     for i in range(len(devices)):
                         print(f"{i}) {devices[i].properties[key]}")
-                        option = input("Choose an option: ")
-                        if option.isdigit() and 0 <= int(option) <= len(devices) - 1:
-                            device = devices[int(option)]
-                            print("Backupping to " + devices[int(option)].properties[key])
-                            break
-                        else:
-                            print("Invalid option")
-    else:
-        error("target_label or target_path must be given to find your device")
+                    option = input("Choose an option: ")
+                    if option.isdigit() and 0 <= int(option) <= len(devices) - 1:
+                        device = devices[int(option)]
+                        print("Backupping to " + devices[int(option)].properties[key])
+                        break
+                    else:
+                        print("Invalid option")
 
     # get device path to write
-    if not isinstance(device.device_node, str):
+    if type(device.device_node) is not str:
         error("The device directory couldn't be found")
     mounter = subprocess.run("udisksctl mount -b " + device.device_node, capture_output=True, text=True)
     # output is "mounted /dev/... in /path/to/device" if it got mounted, otherwise "...already mounted in `/path/to/device'."
@@ -92,73 +99,90 @@ else:
 def path_in_target(relative: str)->str:
     return path.join(target_path, relative)
 
-if check_option("directory", str):
-    target_path = path_in_target(config.directory)
+target_directory = get_key("directory", str)
+if target_directory is not None:
+    target_path = path_in_target(target_directory)
+
+# prepare environment
+env = os.environ.copy()
+env.pop("LD_LIBRARY_PATH", None) # Remove PyInstaller’s injected paths
+env.pop("LD_PRELOAD", None)
+env["PATH"] = "/usr/bin:/bin"
 
 # borg helper
 borg_command = ["borg"]
-if check_option("progress", bool):
-    borg_command.append(config.progress)
-def borg(args: list[str])->subprocess.CompletedProcess|typing.Never:
-    process = subprocess.run(borg_command + args)
+if get_key("progress", bool):
+    borg_command.append("--progress")
+def borg(args: list[str])->None|typing.Never:
+    process = subprocess.run(borg_command + args, env=env)
     if process.returncode != 0:
         print("Borg exited with error")
         sys.exit(process.returncode)
-    return process
 
 # configure repo
 repo_path = path_in_target("repo")
-quota_exists = check_option("quota", float)
+quota = get_key("quota", float)
+quota_exists = quota is not None
+quota_config = path_in_target("quota")
+def set_repo_quota() -> None | typing.Never:
+    try:
+        with open(quota_config, "w") as f:
+            f.write(str(quota))
+    except OSError as e:
+        error("This error was raised while trying to write in the repo's quota file: " + str(e))
 if path.exists(repo_path):
     borg(["check", repo_path])
 
-    quota_config = path_in_target("quota")
     repo_quota = None
     if path.exists(quota_config):
         try:
-            repo_quota = float(open(quota_config).read())
+            with open(quota_config) as f:
+                repo_quota = float(f.read())
         except ValueError:
             print("The repo's quota file held a value that could not be parsed")
-            print("Please do not modify the repo's files manually")
+        except OSError as e:
+            error("This error was raised while trying to read the repo's quota file: " + str(e))
     def change_quota(quota)->None:
-        borg(["config", repo_path, "storage_quota", str(quota)])
+        borg(["config", repo_path, "storage_quota", f"{quota}G"])
     if quota_exists:
-        if not repo_quota or config.quota != repo_quota:
-            change_quota(config.quota)
-    elif quota_exists:
+        if not repo_quota or quota != repo_quota:
+            change_quota(quota)
+            set_repo_quota()
+    elif repo_quota:
         change_quota(0)
+        os.remove(quota_config)
 else:
     initializer = ["init", "-e", "none", repo_path]
     if quota_exists:
-        initializer += ["--storage-quota", config.quota]
+        initializer += ["--storage-quota", f"{quota}G"]
+        set_repo_quota()
     borg(initializer)
 
-borg_backup = list(borg_command)
-if check_option("stats", bool):
-    borg_backup += ["-s"]
-borg_backup += ["create", "-C", config.compression if check_option("compression", str) else "lz4"]
+borg_backup = list()
+compression = get_key("compression", str)
+borg_backup += ["create", "-C", "lz4" if compression is None else compression]
+if get_key("stats", bool):
+    borg_backup.append("-s")
 locale.setlocale(locale.LC_TIME, "")
-archive_name = f"{repo_path}::{datetime.now().strftime("%x-%X")}"
+now = datetime.now()
+archive_name = f"{repo_path}::{now.strftime("%x").replace("/", ".")}-{now.strftime("%H.%M.%S")}-"
 backup_n = 0
 gits: list[str] = list()
 """The number of repos already backupped"""
 def backup(repo: dict[str,typing.Any])->None|typing.Never:
     global backup_n
-    def check_key(key: str, typ: type)->bool|typing.Never:
-        if key in repo:
-            if isinstance(repo[key], typ):
-                return True
-            error(f"'{key}' in Repo must be of type '{typ}'")
-        return False
-    def check_list(key: str)->bool|typing.Never:
-        if check_key(key, list):
-            if len({type(i) for i in repo[key]}) == 1:
-               return True
-            error(f"An item in a '{key}' list was not of type str")
-        return False
+    def get_repo_key(key: str, typ: type)->typing.Any|None|typing.Never:
+        return get_key(key, typ, repo)
+    def check_list(key: str)->list|None|typing.Never:
+        lis = get_repo_key(key, list)
+        if lis is None:
+            return None
+        if all(type(i) is str for i in lis):
+           return lis
+        error(f"An item in a '{key}' list was not of type str")
 
     # get path
-    if not check_key("path", str):
+    if not get_repo_key("path", str):
         error("A 'path' value must be defined for each archive")
     repo_path = path.join(repo["path"])
 
@@ -175,35 +199,54 @@ def backup(repo: dict[str,typing.Any])->None|typing.Never:
             backup(new_repo)
         return
 
-    args = [f"{archive_name}-{backup_n}"]
-    if check_list("include"):
-        for glob_path in repo["include"]:
-            args += [*glob(path.join(repo_path, glob_path))]
-    else:
+    def include(lines: list[str]) -> None:
+        spec = PathSpec.from_lines("gitwildmatch", lines)
+        for root, _, files in os.walk(repo_path):
+            for name in files:
+                full_path = path.join(root, name)
+                rel_path = path.relpath(full_path, repo_path)
+                if spec.match_file(rel_path):
+                    args.append(full_path)
+
+    args = [archive_name + str(backup_n)]
+    included = check_list("include")
+    if included is None:
         args.append(repo_path)
-    if check_key("git", bool) and repo["git"]:
+    else:
+        include(included)
+    if get_repo_key("git", bool):
         args.append(path.join(repo_path, ".git"))
         gits.append(repo_path)
-    if check_list("exclude"):
-        for pattern in repo["exclude"]:
+    excluded = check_list("exclude")
+    if excluded is not None:
+        for pattern in excluded:
             args += ["-e", pattern]
 
     borg(borg_backup + args)
     backup_n += 1
-if check_option("repos", list):
-    for repo in config.repos:
+
+repos = get_key("repos", list)
+if repos:
+    for repo in repos:
+        if type(repo) is not dict:
+            error(f"Repo number {repos.index(repo) + 1} is not a dictionary")
+    for repo in repos:
         backup(repo)
 else:
     error("'repos' must be set to backup your repositories")
 # git directories
-with open(path_in_target("git_directories"), "w") as f:
-    f.write(str(gits))
+try:
+    with open(path_in_target("git_directories"), "w") as f:
+        f.write(str(gits))
+except OSError as e:
+    error("This error was raised while trying to write on the repo's git_directories file: " + str(e))
 
 # compact repo
-if check_option("max_archives", int):
+max_archives = get_key("max_archives", int)
+if max_archives is not None:
     archives_number = subprocess.run(["borg", "list", "--short", repo_path],
-                                     capture_output=True, text=True
-                                     ).stdout.count("\n") + 1
-    if archives_number > config.max_archives:
-        borg(["delete", repo_path, "--first", str(archives_number - config.max_archives)])
-        borg(["compact"])
+                                     capture_output=True, text=True, env=env
+                                     ).stdout.count("\n")
+    if archives_number > max_archives:
+        borg(["delete", repo_path, "--first", str(archives_number - max_archives)])
+        borg(["compact", repo_path])
