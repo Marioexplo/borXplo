8ed3ff8a426344988498d92caaf5458084c8e060
Marioexplo <marioexplo@protonmail.com>
2026-04-04 13:10:04 +0200

    main.py doesn't repeat in backupper's thread

--- main.py
+++ main.py
@@ -39,41 +39,43 @@ def gui() -> None:
 
 HOME = path.expanduser("~")
 CONFIG = path.join(HOME, ".config/borXplo")
-if len(argv) == 1 or argv[1][:2] == "--":
-    import backup
-    def printer(text: str) -> None:
-        print(text)
-        print()
-    if "--gui" in argv:
-        gui()
-    else:
-        backup.main(False)
-        exit()
+if __name__ == "__main__":
+    if len(argv) == 1 or argv[1][:2] == "--":
+        def printer(text: str) -> None:
+            print(text)
+            print()
+        if "--gui" in argv:
+            import gui
+            gui.main()
+        else:
+            import backup
+            backup.main(False)
+            exit()
 
-APP_FILES: str
-match argv[1]:
-    case "help" | "guide":
-        with open(path.join(APP_FILES, "help.txt" if argv[1] == "help" else "config.guide.txt")) as help:
-            print(help.read())
-    case "automatic":
-        if len(argv) == 2:
-            error("An integer must be given with option 'automatic'")
-        n_str = argv[2]
-        try:
-            n = int(n_str)
-        except ValueError:
-            error(n_str + " is not an integer")
-        AUTOSTART = path.join(HOME, ".config/autostart/borxplo.desktop")
-        if n > 0:
-            if not path.exists(AUTOSTART):
-                with open(path.join(APP_FILES, "automatic.desktop")) as f:
-                    write(AUTOSTART, f.read())
-            write(path.join(CONFIG, "automatic"), n_str)
-        elif path.exists(AUTOSTART):
-            if path.isfile(AUTOSTART):
-                remove(AUTOSTART)
-            else:
-                error(AUTOSTART + " was not expected to be a directory")
-    case "check":
-        import check
-        check.main()
+    APP_FILES: str
+    match argv[1]:
+        case "help" | "guide":
+            with open(path.join(APP_FILES, "help.txt" if argv[1] == "help" else "config.guide.txt")) as help:
+                print(help.read())
+        case "automatic":
+            if len(argv) == 2:
+                error("An integer must be given with option 'automatic'")
+            n_str = argv[2]
+            try:
+                n = int(n_str)
+            except ValueError:
+                error(n_str + " is not an integer")
+            AUTOSTART = path.join(HOME, ".config/autostart/borxplo.desktop")
+            if n > 0:
+                if not path.exists(AUTOSTART):
+                    with open(path.join(APP_FILES, "automatic.desktop")) as f:
+                        write(AUTOSTART, f.read())
+                write(path.join(CONFIG, "automatic"), n_str)
+            elif path.exists(AUTOSTART):
+                if path.isfile(AUTOSTART):
+                    remove(AUTOSTART)
+                else:
+                    error(AUTOSTART + " was not expected to be a directory")
+        case "check":
+            import check
+            check.main()
