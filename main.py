f11f38be909463bde0cd9ece97cea41b055d0bd4
Marioexplo <marioexplo@protonmail.com>
2026-04-05 19:51:59 +0200

    Fixed main double-executing

--- main.py
+++ main.py
@@ -22,50 +22,57 @@ def write(path: str, text: str) -> None | typing.Never:
 
 HOME = path.expanduser("~")
 CONFIG = path.join(HOME, ".config/borXplo")
-if len(argv) == 1 or argv[1][:2] == "--":
-    if "--gui" in argv:
-        import gui
-        gui.main()
-    else:
-        import backup
-        backup.main()
-    exit()
+if __name__ == "__main__":
+    if len(argv) == 1 or argv[1][:2] == "--":
+        if "--gui" in argv:
+            import gui
+            gui.main()
+        else:
+            import backup
+            backup.main()
+        exit()
 
-APP_FILES: str
-match argv[1]:
-    case "help" | "guide":
-        with open(path.join(APP_FILES, "help.txt" if argv[1] == "help" else "config.guide.txt")) as help:
-            print(help.read())
-    case "automatic":
-        if len(argv) == 2:
-            error("An integer must be given with option 'automatic'")
+    APP_FILES: str
+    match argv[1]:
+        case "help" | "guide":
+            with open(path.join(APP_FILES, "help.txt" if argv[1] == "help" else "config.guide.txt")) as help:
+                print(help.read())
 
-        n_str = argv[2]
-        try:
-            n = int(n_str)
-        except ValueError:
-            error(n_str + " is not an integer")
+        case "automatic":
+            if len(argv) == 2:
+                error("An integer must be given with option 'automatic'")
 
-        terminal = False
-        if len(argv) == 4:
-            if argv[3] in ("0", "true", "terminal"):
-                terminal = True
-            else:
-                error('Invalid input to enable the terminal option. Only "terminal", "true" and "0" are accepted')
+            n_str = argv[2]
+            try:
+                n = int(n_str)
+            except ValueError:
+                error(n_str + " is not an integer")
 
-        AUTOSTART = path.join(HOME, ".config/autostart/borxplo.desktop")
-        if n > 0:
-            if not path.exists(AUTOSTART):
-                with open(path.join(APP_FILES, "automatic.desktop")) as f:
-                    write(AUTOSTART, f.read() + ("true" if terminal else "false"))
-            write(path.join(CONFIG, "automatic"), n_str)
-        elif path.exists(AUTOSTART):
-            if path.isfile(AUTOSTART):
-                remove(AUTOSTART)
-            else:
-                error(AUTOSTART + " was not expected to be a directory")
-    case "check":
-        import check
-        check.main()
-    case _:
-        error("Invalid command")
+            terminal = False
+            if len(argv) == 4:
+                if argv[3] in ("0", "true", "terminal"):
+                    terminal = True
+                else:
+                    error('Invalid input to enable the terminal option. Only "terminal", "true" and "0" are accepted')
+
+            if len(argv) > 4:
+                error("Too many arguments")
+
+            AUTOSTART = path.join(HOME, ".config/autostart/borxplo.desktop")
+            if n > 0:
+                if not path.exists(AUTOSTART):
+                    with open(path.join(APP_FILES, "automatic.desktop")) as f:
+                        write(AUTOSTART, f.read() + ("true" if terminal else "false"))
+                write(path.join(CONFIG, "automatic"), n_str)
+            elif path.exists(AUTOSTART):
+                if path.isfile(AUTOSTART):
+                    remove(AUTOSTART)
+                else:
+                    error(AUTOSTART + " was not expected to be a directory")
+
+        case "check":
+            import check
+            check.main()
+
+        case _:
+            error("Invalid command")
